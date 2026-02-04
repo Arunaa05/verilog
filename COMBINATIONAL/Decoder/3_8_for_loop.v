ef2e340d76051c81e4e0088c289c88617e242ad1
Aruna Annadurai <arunaparimala76@gmail.com>
2026-02-04 23:26:41 +0530

    Update 3_8_for_loop.v

--- COMBINATIONAL/Decoder/3_8_for_loop.v
+++ COMBINATIONAL/Decoder/3_8_for_loop.v
@@ -11,3 +11,25 @@ module decoder3_8(input x2,x1,x0,output reg[7:0]y);
   end
     
 endmodule
+
+//testbench
+
+module decoder_test;
+  reg x2,x1,x0;
+  wire [7:0]y;
+  decoder3_8 dut(.*);
+  initial begin 
+    x2=0;x1=0;x0=0;
+    #10x2=0;x1=0;x0=1;
+    #10x2=0;x1=1;x0=0;
+    #10x2=0;x1=1;x0=1;
+    #10x2=1;x1=0;x0=0;
+    #10x2=1;x1=0;x0=1;
+    #10x2=1;x1=1;x0=0;
+    #10x2=1;x1=1;x0=1;
+    #50$finish;
+  end
+  initial begin
+    $monitor("time=%0t|x2=%b x1=%b x0=%b|y=%b",$time,x2,x1,x0,y);
+  end
+endmodule
