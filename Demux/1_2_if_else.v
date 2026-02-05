33a036e12a77625d356e21c72f510f59f2b09121
Aruna Annadurai <arunaparimala76@gmail.com>
2026-02-05 10:13:33 +0530

    Update 1_2_if_else.v

--- Demux/1_2_if_else.v
+++ Demux/1_2_if_else.v
@@ -10,3 +10,21 @@ module demux1_2(input x,s,output reg y0,y1);
     end
   end
 endmodule
+
+//testbench
+
+module demux_test;
+  reg x,s;
+  wire yo,y1;
+  demux1_2 dut(.*);
+  initial begin
+    s=0;x=0;
+    #20s=0;x=1;
+    #20s=1;x=0;
+    #20s=1;x=1;
+    $stop;
+  end
+  initial begin
+    $monitor("time =%0t | s=%b  x=%b | y0=%b y1=%b",$time,s,x,y0,y1);
+  end
+endmodule
