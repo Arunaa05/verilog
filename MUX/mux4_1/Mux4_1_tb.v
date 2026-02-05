2626d7ee2b6da69fb79023151bc81d39f45253cb
Aruna Annadurai <arunaparimala76@gmail.com>
2026-02-05 13:13:40 +0530

    Update Mux4_1_tb.v

--- MUX/mux4_1/Mux4_1_tb.v
+++ MUX/mux4_1/Mux4_1_tb.v
@@ -23,8 +23,29 @@ module mux_test;
       for(i=0;i<16;i=i+1)
       #10{i0,i1,i2,i3}=i;
     end
+    $finish;
   end
   initial begin
     $monitor("time=%0t|i0=%b i1=%b i2=%b i3=%b| s0=%b s1=%b| y=%b",$time,i0,i1,i2,i3,s0,s1,y);
+  end
+endmodule
+
+//testbench
+
+module mux_test;
+  reg i0,i1,i2,i3,s1,s0;
+  wire y;
+  integer i;
+  mux4_1 dut(.*);
+  initial begin
+    i0=0;i1=1;i2=1;i3=0;
+    for(i=0;i<4;i=i+1)begin
+      {s1,s0}=i;
+      #10;
+    end
+    #40$finish;
+  end
+   initial begin
+     $monitor("time =%0t | i0=%b  i1=%b i2=%b i3=%b | s1=%b s0=%b | y=%b",$time,i0,i1,i2,i3,s1,s0,y);
   end
 endmodule
