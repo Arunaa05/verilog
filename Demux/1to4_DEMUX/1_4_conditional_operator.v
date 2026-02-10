3b9f8fb370a0cbe0631274d7ed91f110d465c56c
Aruna Annadurai <arunaparimala76@gmail.com>
2026-02-10 20:16:57 +0530

    Update 1_4_conditional_operator.v

--- Demux/1to4_DEMUX/1_4_conditional_operator.v
+++ Demux/1to4_DEMUX/1_4_conditional_operator.v
@@ -1,3 +1,7 @@
 module(input x,s0,s1,output y0,y1,y2,y3);
-  assign y0=
+  assign y0=(s0==0&s1==0)?x;
+  assign y1=(s0==0&s1==1)?x;
+  assign y2=(s0==1&s1==0)?x;
+  assign y3=(s0==1&s1==1)?x;
+endmodule
   
