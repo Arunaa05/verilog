6467fec259e49e15ee67a4b82bfd8a931de56731
Aruna Annadurai <arunaparimala76@gmail.com>
2026-02-11 09:46:39 +0530

    Create SRlatch_nand_tb.v

---
+++ SEQUENTIAL/Latches/SRlatch_NAND/SRlatch_nand_tb.v
@@ -0,0 +1,15 @@
+module srlatch_test;
+  reg s,r;
+  wire q,qbar;
+  srlatch_nand dut(.*);
+  initial begin
+    s=0;r=0;
+    #20 s=0;r=1;
+    #20 s=1;r=0;
+    #20 s=1;r=1;
+    #50$finish;
+  end
+  initial begin 
+    $monitor("time=%0t |s=%b r=%b|q=%b qbar=%b",$time,s,r,q,qbar);
+  end
+endmodule
