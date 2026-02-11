959f1cabed4bf052a25aec61641ead90f11182bd
Aruna Annadurai <arunaparimala76@gmail.com>
2026-02-11 17:00:41 +0530

    Update and rename JK_FlipFlop to JK_FlipFlop.v

---
+++ SEQUENTIAL/FlipFlops/JK_FlipFlop.v
@@ -0,0 +1,11 @@
+module jk_ff(input j,k,clk,output q,qbar);
+  assign qbar=~q;
+  always@(posedge clk)begin
+    case({j,k})
+      2'b00:q<=q;
+      2'b01:q<=0;
+      2'b10:q<=1;
+      2'b11:q<=~q;
+    endcase
+  end
+endmodule
