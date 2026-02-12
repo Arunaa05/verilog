75561658ca8cb815544cd2df7fd6f869fde9037b
Aruna Annadurai <arunaparimala76@gmail.com>
2026-02-12 10:06:09 +0530

    Update JK_Flipflop_tb.v

--- SEQUENTIAL/FlipFlops/JK_Flipflop_tb.v
+++ SEQUENTIAL/FlipFlops/JK_Flipflop_tb.v
@@ -1,1 +1,22 @@
-
+module jkff_tb;
+  reg j,k,clk;
+  wire q,qbar;
+  jk_ff dut(.*);
+  initial 
+    clk=0;
+  always #5 clk=~clk;
+  initial begin
+    j=1;k=0;
+    #10 j=0;k=1;
+    #10 j=1;k=1;
+    #10 j=0;k=1;
+    #10 j=0;k=0;
+    #20; $finish;
+  end
+  initial begin 
+    $monitor("time=%0t | clk=%b j=%b k=%b | q=%b qbar=%b",$time,clk,j,k,q,qbar);
+    $dumpfile("jk_ff.vcd");
+    $dumpvars;
+  end
+endmodule
+    
