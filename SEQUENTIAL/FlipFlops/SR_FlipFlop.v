d383a7b9c2853f00231c5b288dcfb19054d36fa6
Aruna Annadurai <arunaparimala76@gmail.com>
2026-02-12 10:17:20 +0530

    Update SR_FlipFlop.v

--- SEQUENTIAL/FlipFlops/SR_FlipFlop.v
+++ SEQUENTIAL/FlipFlops/SR_FlipFlop.v
@@ -1,8 +1,16 @@
 module sr_ff(
-  input s,r,clk,rst,
+  input s,r,clk,
   output reg q,qbar
 );
-  always@(posedge clk or posedge rst)begin
-    if(rst)
-      q<=1'b0;
+  assign qbar =~q;
+  always@(posedge clk)begin
+    case({s,r})
+      2'b00:q<=q;
+      2'b01:q<=0;
+      2'b10:q<=1;
+      2'b11:q<=1'bx;
+    endcase
+  end
+endmodule
+    
     
