0d3d4a3f662773919ab16546afaad6a306335efd
Aruna Annadurai <arunaparimala76@gmail.com>
2026-02-16 16:39:19 +0530

    Update 4bit_adder_tb.v

--- Combinational/Adder/4_bit_adder/4bit_adder_tb.v
+++ Combinational/Adder/4_bit_adder/4bit_adder_tb.v
@@ -1,26 +1,19 @@
 module adder_4bit_tb;
 
   reg [3:0]a, b;
-  reg cin;
   wire [3:0]sum;
-  wire cout;
 
 adder_4bit dut(.*);
 
 initial begin
   
-  a=4'b0000;b=4'b0001;cin=0;
-  #10 a=4'b0000;b=4'b0001;cin=1;
-  #10 a=4'b0110;b=4'b0001;cin=0;
-  #10 a=4'b0110;b=4'b0001;cin=1;
-  #10 a=4'b1000;b=4'b0010;cin=0;
-  #10 a=4'b1000;b=4'b0010;cin=1;
-  #10 a=4'b1111;b=4'b0101;cin=0;
-  #10 a=4'b1111;b=4'b0101;cin=1;
+  a=4'd6;b=4'd5;
+  #10 a=4'd10;b=4'd6;
+  #10 a=4'd5;b=4'd9;
   #50 $finish;
 end
   initial begin
-    $monitor("time=%0t a=%b b=%b cin=%b sum=%b cout=%b",$time,a,b,cin,sum,cout);
+    $monitor("time=%0t a=%b b=%b sum=%b ",$time,a,b,sum);
   end
 
 endmodule
