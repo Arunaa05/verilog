33351565e4ca92be17a61d9a4162d2db11b43c87
Aruna Annadurai <arunaparimala76@gmail.com>
2026-02-16 17:58:35 +0530

    Create d_ff_tb.v

---
+++ Sequential/FlipFlops /D_ff/d_ff_tb.v
@@ -0,0 +1,23 @@
+module dff_tb;
+reg d;
+reg clk;
+reg rst;
+wire q;  
+  d_ff dut(.*);
+  always #10 clk=~clk;
+  initial begin
+    clk=0;
+    d=1;
+    #10
+    clk=1;
+    d=1;
+    #10 d=0;
+    #10 d=1;
+    #50 $finish;
+  end
+  initial begin
+    $monitor("time=%0t | clk=%b | d=%b |q=%b",$time,clk,d,q);
+    $dumpfile("d_ff.vcd");
+    $dumpvars(1,dff_tb);
+  end
+endmodule
