e7cc198137fd891daca7e3f51adca801644ff716
Aruna Annadurai <arunaparimala76@gmail.com>
2026-02-16 16:56:22 +0530

    Update 4bit_sub.v

--- Combinational/Subtractor/4_bit_subtractor/4bit_sub.v
+++ Combinational/Subtractor/4_bit_subtractor/4bit_sub.v
@@ -1,10 +1,8 @@
 module sub_4bit(
   input [3:0]a,b,
-  input cin,
-  output reg [3:0]diff,
-  output reg bout
+  output reg [3:0]diff
 );
   always@(*)begin
-    {bout,diff}=a-b-cin;
+     diff=a-b;
   end
 endmodule
