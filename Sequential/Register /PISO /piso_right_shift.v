91e1d439eb5a23b0ac8e28bed220fd793165ed9b
Aruna Annadurai <arunaparimala76@gmail.com>
2026-02-17 21:35:55 +0530

    Create piso_right_shift.v

---
+++ Sequential/Register /PISO /piso_right_shift.v
@@ -0,0 +1,17 @@
+module piso(
+  input clk,rst,load,
+  input [3:0]din,
+  output reg dout
+);
+  reg [3:0]q;
+  always@(posedge clk)begin
+    if(rst)
+      q<=4'b0000;
+    else if(load)
+      q<=din;
+    else begin
+      dout<=q[0];
+      q<={1'b0,q[2:0]};
+    end
+  end
+endmodule
