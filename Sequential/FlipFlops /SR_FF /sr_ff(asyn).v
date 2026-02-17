d33294de36228221968e1a51bc9dda7962d252e9
Aruna Annadurai <arunaparimala76@gmail.com>
2026-02-17 10:51:32 +0530

    Create sr_ff(asyn).v

---
+++ Sequential/FlipFlops /SR_FF /sr_ff(asyn).v
@@ -0,0 +1,14 @@
+module sr_ff(input s,r,clk,rst, output reg q);
+  always@(posedge clk or posedge rst)begin
+    if(rst)
+      q<=1'b0;
+    else begin
+      case({s,r})
+        0:q<=q;
+        1:q<=0;
+        2:q<=1;
+        default:q<=1'bx;
+      endcase
+    end
+  end
+endmodule
