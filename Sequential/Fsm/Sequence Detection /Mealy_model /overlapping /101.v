4b175153ac8f105a381dfeb4c2651abf90202b64
Aruna Annadurai <arunaparimala76@gmail.com>
2026-02-18 12:02:58 +0530

    Update 101.v

--- Sequential/Fsm/Sequence Detection /Mealy_model /overlapping /101.v
+++ Sequential/Fsm/Sequence Detection /Mealy_model /overlapping /101.v
@@ -1,1 +1,42 @@
-
+module seq_101(input clk,rst,x,output reg z);
+  reg [1:0]state,next_state;
+  parameter s0=2'd0,
+            s1=2'd1,
+            s2=2'd2;
+  always@(posedge clk)begin
+    if(rst)
+      state<=s0;
+    else
+      state<=next_state;
+  end
+  always@(*)begin
+    next_state=state;
+    case(state)
+      s0:begin
+        if(x==1)
+          next_state=s1;
+        else
+          next_state=s0;
+      end
+      s1:begin
+        if(x==0)
+          next_state=s2;
+        else
+          next_state=s1;
+      end
+      s2:begin
+        if(x==1) begin
+          next_state=s1;
+        end
+        else
+          next_state=s0;
+      end
+    endcase
+  end
+  always@(posedge clk)begin
+      if(state==s2)
+        z<=1;
+      else
+        z<=0;
+    end
+endmodule
