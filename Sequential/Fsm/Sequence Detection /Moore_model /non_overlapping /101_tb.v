8c75f8c2f8cae10901dbb3ec0cac08ebba04997e
Aruna Annadurai <arunaparimala76@gmail.com>
2026-02-18 12:23:19 +0530

    Create 101_tb.v

---
+++ Sequential/Fsm/Sequence Detection /Moore_model /non_overlapping /101_tb.v
@@ -0,0 +1,24 @@
+module seq_101_tb();
+  reg clk,rst,x;
+  wire z;
+  seq_101 dut (.*);
+    always #5 clk=~clk;
+    initial begin
+       clk = 0;
+       rst = 1;
+       x   = 0;
+    #10 rst = 0;
+    #10 x = 1;
+    #10 x = 0;
+    #10 x = 1;   
+    #10 x = 0;
+    #10 x = 1;   
+    #20 $finish;
+    end
+  initial begin
+    $monitor("time=%0t, clk=%b  rst=%b x=%b | z=%b state=%b",$time,clk,rst,x,z,dut.state);
+    $dumpfile("seq_101.vcd");
+    $dumpvars(1,seq_101_tb);
+    
+  end
+endmodule
