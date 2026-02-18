90cf84b29f1a4a0e2eea5ef82abf1b126b0f6b14
Aruna Annadurai <arunaparimala76@gmail.com>
2026-02-18 16:29:59 +0530

    Create decoder3_8_tb.v

---
+++ Combinational/Decoder /Decoder3_8 /decoder3_8_tb.v
@@ -0,0 +1,26 @@
+module decoder_3to8_tb;
+
+    reg  [2:0] in;
+    wire [7:0] out;
+
+    decoder_3to8 dut (
+        .in(in),
+        .out(out)
+    );
+
+    initial begin
+      $monitor("Time=%0t | in=%b | out=%b", $time, in, out);
+
+        in = 3'b000; #10;
+        in = 3'b001; #10;
+        in = 3'b010; #10;
+        in = 3'b011; #10;
+        in = 3'b100; #10;
+        in = 3'b101; #10;
+        in = 3'b110; #10;
+        in = 3'b111; #10;
+
+        $finish;
+    end
+
+endmodule
