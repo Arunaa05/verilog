15b058b96adb6ad6ed04403a86daf82cc7a298c6
Aruna Annadurai <arunaparimala76@gmail.com>
2026-03-06 15:55:08 +0530

    Create fifo_sync.v

---
+++ Sequential/FIFO /with_slot /fifo_sync.v
@@ -0,0 +1,38 @@
+module synchronous_fifo #(parameter W=4 ,D=4)(
+  input clk,rst,
+  input w_en,r_en,
+  input [W-1:0]data_in,
+  output reg [W-1:0]data_out
+);
+  wire full,empty;
+  localparam integer a=$clog2(D);
+  reg [W-1:0]fifo[0:D-1];
+  
+  
+  
+  always@(posedge clk) begin
+    if(rst)begin
+        w_ptr <= 0;
+    end
+    else 
+      if(w_en && !full) begin
+        fifo[w_ptr] = data_in;
+        w_ptr <= w_ptr + 1;
+      end
+  end
+   always@(posedge clk) begin
+     if(rst)begin
+        r_ptr <= 0;
+        data_out<=0;
+     end
+     else
+      if(r_en && !empty ) begin
+        data_out <= fifo[r_ptr];
+        r_ptr <= r_ptr + 1;
+      end
+   end
+  reg [a:0]w_ptr,r_ptr;
+  assign full = (( w_ptr + 1'b1) == r_ptr);
+  assign empty = (w_ptr==r_ptr);
+      
+      endmodule
