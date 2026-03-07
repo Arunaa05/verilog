eb999881f1c32ed6632a09d9bfe85f83d4d7be23
Aruna Annadurai <arunaparimala76@gmail.com>
2026-03-07 17:18:27 +0530

    Create syn_read.v

---
+++ Sequential/Memory /Ram /ram_syn_read /syn_read.v
@@ -0,0 +1,16 @@
+module synchronous_ram(
+  input clk,
+  input [3:0]addr,
+  input [7:0]data_in,
+  input we,
+  output reg [7:0]data_out
+);
+  reg [7:0] mem[0:15];
+  always@(posedge clk)
+    begin
+      if(we)
+        mem[addr]<=data_in;
+      else
+        data_out<=mem[addr];
+    end
+endmodule
