015e13a08d1f743a5c76635fa264df8d5cf697cb
Aruna Annadurai <arunaparimala76@gmail.com>
2026-04-20 23:01:08 +0530

    Create q17.v

---
+++ Sequential/Assignment_3 /q17.v
@@ -0,0 +1,15 @@
+module tb;
+int alpha, beta;
+initial 
+begin
+alpha = 4; beta = 3;
+beta <= beta + alpha;
+alpha <= alpha + beta;
+alpha=alpha+2;
+$display("Alpha=%0d Beta=%0d", alpha, beta);
+end
+endmodule
+
+/*output
+alpha=6 beta =3
+*/
