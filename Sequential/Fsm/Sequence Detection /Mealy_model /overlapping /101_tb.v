f2f3f9dd633bd7930f22e23ad3c111f5466e481c
Aruna Annadurai <arunaparimala76@gmail.com>
2026-04-30 12:10:41 +0530

    Update 101_tb.v

--- Sequential/Fsm/Sequence Detection /Mealy_model /overlapping /101_tb.v
+++ Sequential/Fsm/Sequence Detection /Mealy_model /overlapping /101_tb.v
@@ -22,3 +22,22 @@ module seq_101_tb();
     
   end
 endmodule
+/*output
+time=0, clk=0  rst=1 x=0 | z=x state=xx
+time=5, clk=1  rst=1 x=0 | z=0 state=00
+time=10, clk=0  rst=0 x=0 | z=0 state=00
+time=15, clk=1  rst=0 x=0 | z=0 state=00
+time=20, clk=0  rst=0 x=1 | z=0 state=00
+time=25, clk=1  rst=0 x=1 | z=0 state=01
+time=30, clk=0  rst=0 x=0 | z=0 state=01
+time=35, clk=1  rst=0 x=0 | z=0 state=10
+time=40, clk=0  rst=0 x=1 | z=0 state=10
+time=45, clk=1  rst=0 x=1 | z=1 state=01
+time=50, clk=0  rst=0 x=0 | z=1 state=01
+time=55, clk=1  rst=0 x=0 | z=0 state=10
+time=60, clk=0  rst=0 x=1 | z=0 state=10
+time=65, clk=1  rst=0 x=1 | z=1 state=01
+time=70, clk=0  rst=0 x=1 | z=1 state=01
+time=75, clk=1  rst=0 x=1 | z=0 state=01
+testbench.sv:18: $finish called at 80 (1s)
+time=80, clk=0  rst=0 x=1 | z=0 state=01*/
